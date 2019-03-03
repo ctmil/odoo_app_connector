3ba4b1d4e55c1539d89691ba8d3c14af5a9dfd37
Ignacio Buioli <ibuioli@gmail.com>
2019-03-03 23:33:01 +0000

    fix(style): improve responsive

--- static/src/js/web.js
+++ static/src/js/web.js
@@ -3,12 +3,17 @@ odoo.define('web', function (require) {
 
     $(document).ready(function () {
 	var interval = setInterval(function(){
-		if($('nav.o_main_navbar').length > 0 && $('.o_menu_sections').length > 0) {
+		if($('nav.o_main_navbar').length > 0 && $('.o_menu_sections').length > 0 && $('.o_menu_systray').length > 0) {
 			if($('#o_button_toggle').length == 0) {
 				$('nav.o_main_navbar').prepend('<button type="button" class="fa fa-bars navbar-toggler" id="o_button_toggle"></button>');
 				$('#o_button_toggle').click(function(){
-					$('.o_menu_sections').animate({height:"toggle"});
-					$('.o_menu_systray').toggle();
+					if($('.o_menu_sections').css("display") == 'none'){
+						$('.o_menu_sections').css("display", 'block');
+						$('.o_menu_systray').css("display", 'block');
+					}else{
+						$('.o_menu_sections').css("display", 'none');
+                                                $('.o_menu_systray').css("display", 'none');
+					}
 				});
 			}
 			clearInterval(interval);
